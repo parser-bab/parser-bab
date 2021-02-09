71fbe34ea15906932ba1e5d4e052ff81f3e7dc44
root <root@parser-bab.com>
2021-02-09 14:38:41 +0000

    dump 09202

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -34,7 +34,7 @@
                     <a class="btn btn-primary" href="{{route('PersonalCabinet')}}">На главную</a>
                 </div>
                 <div class="d-flex justify-content-center">
-                    {{ $lists->links() }}
+                    {{ $lists->onEachSide(3)->links() }}
                 </div>
                 <div class="table-responsive">
                     <table id="mytable" class="table table-hover table-dark table-hover">
@@ -116,7 +116,7 @@
                     </table>
                 </div>
                 <div class="d-flex justify-content-center">
-                    {{ $lists->links() }}
+                    {{ $lists->onEachSide(3)->links() }}
                 </div>
             </div>
         </div>
